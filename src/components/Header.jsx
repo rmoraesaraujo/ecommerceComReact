8159ee97c752bb5fbf601e24614133f7c1072d9b
rmoraesaraujo <renan290812@gmail.com>
2025-06-26 11:44:54 -0300

    Integração com FontAwesome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import logo from '../assets/logo/hashtag.svg';
+import UserButtons from '../utils/userButtons.jsx';
 
 const Header = () => {
   return (
-    <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-9 py-4">
+    <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-9 py-4 items-end justify-between text-base ">
       <a href="/">
-        <img className='h-16 px-2' src={logo} alt="Logo da Magazine" />
+        <img className="h-16 px-2" src={logo} alt="Logo da Magazine" />
       </a>
+
+      <UserButtons />
     </header>
   );
 };
