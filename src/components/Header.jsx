ddab488464cb0c5b79560e3a5aa5ec9ecdadd352
rmoraesaraujo <renan290812@gmail.com>
2025-08-24 22:33:25 -0300

    Implementação do Header dinâmico

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import logo from '../assets/logo/hashtag.svg';
 import UserButtons from '../utils/userButtons.jsx';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
-  return (
+  const { pathname } = useLocation();
+  const completeHeader = (
     <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-9 py-4 items-end justify-between text-xl z-10">
       <a href="/">
         <img className="h-16 px-2" src={logo} alt="Logo da Magazine" />
@@ -11,6 +13,16 @@ const Header = () => {
       <UserButtons />
     </header>
   );
+
+  const simpleHeader = (
+    <header className="flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-9 py-4 items-end justify-between text-xl z-10">
+      <a href="/">
+        <img className="h-12 px-2" src={logo} alt="Logo da Magazine" />
+      </a>
+    </header>
+  );
+
+  return <>{pathname === '/' ? completeHeader : simpleHeader}</>;
 };
 
 export default Header;
